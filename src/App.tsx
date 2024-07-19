7e1f2114aba130c1655c3e84525b53904d65dc5e
Daniela Alves <danieladossantosalves01@gmail.com>
2024-07-19 14:47:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import styles from './app.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello, React with Sass and Vite!</h1>
+    </div>
+  );
+}
+
+export default App;
+
