3e4223081200e7e804b6456cfede4f921d45d815
Daniela Alves <danieladossantosalves01@gmail.com>
2024-07-21 12:38:24 -0300

    feat: Remove Apresentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // src/App.jsx
 import styles from './app.module.scss';
-import Apresentation from './components/Apresentation';
 import Header from "./components/Header";
 function App() {
   return (
