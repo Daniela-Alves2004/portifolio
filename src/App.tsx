d34b9dce3713587875e17a4e99ebae9e9aa687bd
Daniela Alves <danieladossantosalves01@gmail.com>
2024-07-21 13:33:45 -0300

    feat:Apresentation Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // src/App.jsx
 import styles from './app.module.scss';
+import Apresentation from './components/Apresentation';
 import Header from "./components/Header";
 function App() {
   return (
     <div className={styles.container}>
      <Header/>
+     <Apresentation/>
     </div>
   );
 }
